ad0291314e5c2c1e101ecc44006c5dc80f3494e7
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-24 18:03:00 +0200

    Removed using

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,7 +1,6 @@
 global using BlazorEcommerce.Shared;
 global using Microsoft.EntityFrameworkCore;
 using BlazorEcommerce.Server.Data;
-using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
