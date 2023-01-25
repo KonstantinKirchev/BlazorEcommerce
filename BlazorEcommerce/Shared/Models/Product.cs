6e0b3f2300456dfc40262d70cc2bd000decfadd7
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-25 16:23:57 +0200

    Add product type and variants and remove the price from product

--- BlazorEcommerce/Shared/Models/Product.cs
+++ BlazorEcommerce/Shared/Models/Product.cs
@@ -12,11 +12,10 @@ namespace BlazorEcommerce.Shared.Models
 
         public string ImageUrl { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
-
         public int CategoryId { get; set; }
 
         public Category? Category { get; set; }
+
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
     }
 }
