4d389b223b978bb17fc3e42d9b1bbc74184baecc
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-25 13:25:50 +0200

    Add product detail page and some global css

---
+++ BlazorEcommerce/Client/Pages/ProductDetailsBase.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorEcommerce.Client.Shared
+{
+    using Microsoft.AspNetCore.Components;
+    using System.Threading.Tasks;
+
+    public class ProductDetailsBase : ComponentBase
+    {
+        protected Product? product = null;
+        protected string message = string.Empty;
+
+        [Inject]
+        public IProductService? ProductService { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            try
+            {
+                message = "Loading product ...";
+                var result = await ProductService.GetProduct(Id);
+                
+                if (!result.Success)
+                {
+                    message = result.Message;
+                }
+                else
+                {
+                    product = result.Data;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
