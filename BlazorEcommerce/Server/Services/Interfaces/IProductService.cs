4a77ae83f0c9d86678f27172d1166031e296a550
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-26 12:06:36 +0200

    Add pagination for product search on server side

--- BlazorEcommerce/Server/Services/Interfaces/IProductService.cs
+++ BlazorEcommerce/Server/Services/Interfaces/IProductService.cs
@@ -5,7 +5,7 @@
         Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsAsync();
         Task<ActionResult<ServiceResponse<Product>>> GetProductAsync(int id);
         Task<ActionResult<ServiceResponse<List<Product>>>> GetProductsByCategoryAsync(string categoryUrl);
-        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+        Task<ServiceResponse<ProductSearchResult>> SearchProducts(string searchText, int page);
         Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText);
         Task<ServiceResponse<List<Product>>> GetFeaturedProducts();
     }
