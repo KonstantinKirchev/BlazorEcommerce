0f76a796e6a7ae2c11a3e92cdc3f85a73808227d
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-31 22:05:02 +0200

    Add server constants

---
+++ BlazorEcommerce/Server/Infrastructure/ServerConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorEcommerce.Server.Infrastructure
+{
+    public static class ServerConstants
+    {
+        public static readonly string ServerErrorAdding = "Error adding data to the database!";
+        public static readonly string ServerErrorRetrieving = "Error retrieving data from the database!";
+        public static readonly string ServerErrorUpdating = "Error updating data at the database!";
+        public static readonly string ServerErrorDeleting = "Error deleting data from the database!";
+    }
+}
